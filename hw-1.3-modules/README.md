259d84c55a5895c80858ed6df216d4ea7ddbd2d1
riadesigner <evgenii.pogrebnyak@gmail.com>
2023-09-08 16:43:23 +1000

    ok

---
+++ hw-1.3-modules/README.md
@@ -0,0 +1,7 @@
+# Super tiny console game: guess my number
+
+ ### To game:
+
+$ guess-number
+
+thx 2 all
---
+++ hw-1.3-modules/guess-number.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+
+const my_hidden_num = Math.floor(Math.random()*100);
+
+const r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });        
+
+const fn = {
+    ask:()=>{
+        r.question("Я загадал число, угадай, какое?", function(answer) {
+            answer = parseInt(answer,10);   
+            
+            if(!answer){
+                console.log('число то назови...')
+                fn.ask();
+                return false;
+            }         
+            
+            console.log(`ты сказал ${answer}?`)                        
+            if(answer>my_hidden_num){
+                console.log(`не, мое число меньше`);
+                fn.ask();                
+            }else if(answer<my_hidden_num){
+                console.log(`не, мое число больше`);
+                fn.ask();                
+            }else if(answer= my_hidden_num){
+                console.log(`ух ты! Угадал!!!, ${answer}=${my_hidden_num} `);
+                r.close();
+            }
+          });  
+    }
+}
+
+fn.ask();
+
+
+
