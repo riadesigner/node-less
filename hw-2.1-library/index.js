b424740449c3f054b9b23baf7dd00b74f609f8a9
riadesigner <evgenii.pogrebnyak@gmail.com>
2023-09-25 16:43:09 +1000

    ok

--- hw-2.1-library/index.js
+++ hw-2.1-library/index.js
@@ -1,87 +1,95 @@
-
+require("dotenv").config()
 const express = require('express')
 const {v4:uuid} = require('uuid')
 
-class Todo{
-    constructor(title="", description="", id = uuid()){
-        this.title = title
-        this.description = description
-        this.id = id
+const PORT = process.env.PORT || 3001
+
+class Book{
+    constructor(opt){
+        this.id = uuid();                
+        this.update(opt)
+        return this;
     } 
+    update(opt){
+        this.title = opt.title || "";
+        this.description = opt.description || "";
+        this.authors = opt.authors || "";
+        this.favorite = opt.favorite || "";
+        this.fileCover = opt.fileCover || "";
+        this.fileName = opt.fileName || "";        
+        return this;
+    }
 }
 
-const store = {
-        todo:[]
-    }
+const store = {books:[]}
 
 const app = express()
-app.use(expreёss.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-// x-www-form-urlencoded вместо raw
+// app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req,res)=>{
     res.status(201)
-    const goto = 'localhost:3000/api/todo';
+    const goto = 'localhost:3000/api/books';
     res.send(`перейдите на страницу: <a href="http://${goto}">${goto}</a>`)     
 })
 
-
-app.get('/api/todo',(req,res)=>{
-    const {todo} = store
+app.get('/api/user/login',(req,res)=>{
+    const user = { id: 1, mail: "test@mail.ru" };
     res.status(201)
-    res.json(todo)
+    res.json(user)
 })
 
-app.get('/api/todo/:id',(req,res)=>{
-    const {todo} = store
+app.get('/api/books',(req,res)=>{
+    const {books} = store
+    res.status(201)
+    res.json(books)
+})
+
+app.get('/api/books/:id',(req,res)=>{    
+    const {books} = store
     const {id} = req.params
-    const idx = todo.findIndex(el=>el.id===id)
+    const idx = books.findIndex(el=>el.id===id)
     if(idx!==-1){
-        res.json(todo[idx])
+        res.json(books[idx])
     }else{
         res.status(404)
-        res.json('404 | страница не найдена')
+        res.json('404 | книга не найдена')
     }
 })
 
-app.post('/api/todo/',(req,res)=>{
-    const {todo} = store
-    const {title, description} = req.body
-    const newTodo = new Todo(title, description)
-    todo.push(newTodo)
+app.post('/api/books/',(req,res)=>{
+    const {books} = store
+    const {title,description,authors,favorite,fileCover,fileName} = req.body
+    const opt = {title,description,authors,favorite,fileCover,fileName}
+    const newBook = new Book(opt)
+    books.push(newBook)
     res.status(201)
-    res.json(newTodo)    
+    res.json(newBook)    
 })
 
-app.put('/api/todo/:id',(req,res)=>{
-    const {todo} = store
+app.put('/api/books/:id',(req,res)=>{
+    const {books} = store
     const {id} = req.params
-    const {title, description} = req.body
-    const idx = todo.findIndex(el=>el.id===id)
-    if(idx !== -1){
-        const newTodo = new Todo(title,description)
-        todo[idx] = {
-            ...todo[idx],
-            title,
-            description
-        } 
+    const {title,description,authors,favorite,fileCover,fileName} = req.body
+    const opt = {title,description,authors,favorite,fileCover,fileName}
+    const idx = books.findIndex(el=>el.id===id)
+    if(idx !== -1){        
+        books[idx].update(opt)
         res.status(201)
-        res.json(newTodo)
+        res.json(books[idx])
     }else{
         res.status(404)
         res.json('404 | страница не найдена')
     }
-
-
 })
 
-app.delete('/api/todo/:id',(req,res)=>{
-    const {todo} = store
+app.delete('/api/books/:id',(req,res)=>{
+    const {books} = store
     const {id} = req.params
-    const idx = todo.findIndex(el=>el.id===id)
+    const idx = books.findIndex(el=>el.id===id)
     if(idx !== -1){
-        todo.splice(idx,1)
+        books.splice(idx,1)
         res.status(201)
         res.json('ok')
     }else{
@@ -91,4 +99,4 @@ app.delete('/api/todo/:id',(req,res)=>{
 })
 
 
-app.listen(3000)
+app.listen(PORT)
